f245512cf4f66ad748af0b402f3f3b2dc9bed28b
SJB <StevenJBurns@users.noreply.github.com>
2017-08-13 15:16:01 -0600

    Added eventhandler for mousedown (not working), all other functionality is good

--- js/pixelart.js
+++ js/pixelart.js
@@ -1,6 +1,6 @@
 
 
-let colorGroups = {
+const colorGroups = {
   "Pink": ["Pink", "LightPink", "HotPink", "DeepPink", "PaleVioletRed", "MediumVioletRed"],
   "Purple": ["Lavender", "Thistle", "Plum", "Orchid", "Violet", "Fuchsia", "Magenta", "MediumOrchid", "DarkOrchid", "DarkViolet", "BlueViolet", "DarkMagenta", "Purple", "MediumPurple", "MediumSlateBlue", "SlateBlue", "DarkSlateBlue", "Indigo"],
   "Red": ["LightSalmon", "Salmon", "DarkSalmon", "LightCoral", "IndianRed", "Crimson", "Red", "FireBrick", "DarkRed"],
@@ -12,49 +12,18 @@ let colorGroups = {
   "Brown": ["Cornsilk", "BlanchedAlmond", "Bisque", "NavajoWhite", "Wheat", "BurlyWood", "Tan", "RosyBrown", "SandyBrown", "GoldenRod", "DarkGoldenRod", "Peru", "Chocolate", "Olive", "SaddleBrown", "Sienna", "Brown", "Maroon"],
   "White": ["White", "Snow", "HoneyDew", "MintCream", "Azure", "AliceBlue", "GhostWhite", "WhiteSmoke", "SeaShell", "Beige", "OldLace", "FloralWhite", "Ivory", "AntiqueWhite", "Linen", "LavenderBlush", "MistyRose"],
   "Grey": ["Gainsboro", "LightGray", "Silver", "DarkGray", "DimGray", "Gray", "LightSlateGray", "SlateGray", "DarkSlateGray", "Black"]
-}
+} // All 140 named CSS colors as per the W3C organized into general color groups
 
-createPixelGrid(24,24);
 createColorPalette();
+setInitialCurrentColor("red");
+createPixelGrid(24,24);
 
-function createPixelGrid(rows, cols){
-  let divGrid = document.getElementById('divGrid');
-  let i = 0;
-
-  for(let r = 0; r < rows; r++){
-    let divRow = document.createElement('div');
-    divRow.className = "row";
-
-    for(let c = 0; c < cols; c++){
-      let cell = document.createElement('div');
-      cell.id = "cell" + longerInt(i);
-      cell.className += "cell";
-      divRow.appendChild(cell);
-      cell.addEventListener("click", handleCellClick);
-      i++
-    }
-    divGrid.appendChild(divRow);
-  }
-}
-
-function longerInt(i){
-  let int = i.toString();
-
-  while(int.length < 4){
-    int = "0" + int;
-  }
-  return int;
-}
 
 function createColorPalette(paletteGroup){
   let divColorSelect = document.getElementById('divColorSelect');
 
   for (let groupName in colorGroups){
     let divColorGroup = document.createElement("div");
-    let h4GroupName = document.createElement("h4");
-
-    //h4GroupName.innerText = groupName;
-    //divColorGroup.appendChild(h4GroupName);
 
     for(let color of colorGroups[groupName]){
       let divColor = document.createElement('div');
@@ -77,8 +46,55 @@ function handlePaletteColorSelect(){
   spanCurrentColor.innerHTML = this.style.backgroundColor;
 }
 
+function setInitialCurrentColor(color){
+  let divCurrentColor = document.getElementById("divCurrentColor");
+  let spanCurrentColor = document.getElementById("spanCurrentColor");
+
+  divCurrentColor.style.backgroundColor = color;
+  spanCurrentColor.innerHTML = color;
+}
+
+function createPixelGrid(rows, cols){
+  let divGrid = document.getElementById('divGrid');
+  let i = 0;
+
+  for(let r = 0; r < rows; r++){
+    let divRow = document.createElement('div');
+    divRow.className = "row";
+
+    for(let c = 0; c < cols; c++){
+      let cell = document.createElement('div');
+      cell.id = "cell" + longerInt(i);
+      cell.className += "cell";
+      cell.addEventListener("click", handleCellClick);
+      cell.addEventListener("mouseenter", handleCellMouseEnter)
+      divRow.appendChild(cell);
+      i++
+    }
+    divGrid.appendChild(divRow);
+  }
+}
+
+function longerInt(i){
+  let int = i.toString();
+
+  while(int.length < 4){
+    int = "0" + int;
+  }
+  return int;
+}
+
 function handleCellClick(){
   let divCurrentColor = document.getElementById("divCurrentColor");
 
   this.style.backgroundColor = divCurrentColor.style.backgroundColor
+}
+
+function handleCellMouseEnter(e){
+  let divCurrentColor = document.getElementById("divCurrentColor")
+
+  if (this.mousedown == true){
+    this.style.backgroundColor = divCurrentColor.style.backgroundColor;
+  }
+  console.log(this.id);
 }
